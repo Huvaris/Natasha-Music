29a07313c998956f770ec278106bcf97a6117b41
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2022-12-06 13:47:42 +0530

    Update __main__.py

--- Natasha/__main__.py
+++ Natasha/__main__.py
@@ -1,15 +1,15 @@
 from pyrogram import idle
 from pyrogram import Client as Bot
-from Nibbi.Modules.cache.clientbot import run
-from Nibbi.config import API_ID, API_HASH, BOT_TOKEN
+from Natasha.Modules.cache.clientbot import run
+from Natasha.config import API_ID, API_HASH, BOT_TOKEN
 
     
 bot = Bot(
-    ":Nibbi:",
+    ":Natasha:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="Nibbi/Plugins")
+    plugins=dict(root="Natasha/Plugins")
 )
 
 bot.start()
